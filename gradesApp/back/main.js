dd19c1ccf4c4317f583866f71b16370f6b2b45b2
Puncher <65789180+Puncher1@users.noreply.github.com>
2022-06-23 14:39:53 +0200

    Login mobile

--- gradesApp/back/main.js
+++ gradesApp/back/main.js
@@ -1,57 +1,82 @@
 require("dotenv").config()
 
 const webdriver = require("selenium-webdriver")
-const { Browser, By} = require("selenium-webdriver")
+const { Browser, By, until} = require("selenium-webdriver")
 const chrome = require("selenium-webdriver/chrome")
 
-function initDriver()
+
+function initDriver(callback)
 {
   let driver = new webdriver.Builder()
     .forBrowser(Browser.CHROME)
-    // .setChromeOptions(new chrome.Options().headless())
+    .setChromeOptions(new chrome.Options().headless())
     .build()
 
-  return driver
+  callback(driver)
+  
 }
 
 
 /**
  * TODO
  * 
- * @param {webdriver.ThenableWebDriver} driver 
+ * @param {CallableFunction} driver 
  */
-function eduMobile(driver)
+function eduMobile(driver, callback)
 {
+  // @ts-ignore
   driver.get(process.env.EDU_MOBILE_URL)
 
   const pinEntry = driver.findElement(By.xpath('//*[@id="inputPin"]'))
+  // @ts-ignore
   pinEntry.sendKeys(process.env.EDU_PIN)
 
   const pinOkBtn = driver.findElement(By.xpath('/html/body/div/form/input[2]'))
   pinOkBtn.click()
 
+  setTimeout(function () 
+  {
+    let mobile_body = driver.findElement(By.xpath('/html/body'))
+    mobile_body.getText().then(function (mobiledBodyText) 
+    {
+      let upToDate = false
+      if (mobiledBodyText.indexOf("Sie haben alle Noten bestätigt.") == -1)
+      {
+        upToDate = true
+      }
 
-  try {
-    var mobile_body = driver.findElement(By.xpath('/html/body')).getText()
-
-  } catch {
-    var mobile_body = driver.findElement(By.xpath('/html/body')).getText()
-  }
-
-  console.log(mobile_body)
+      return upToDate
+    })
+  }, 5000)
 }
 
+/**
+ * 
+ * @param {webdriver.ThenableWebDriver} driver 
+ * @param {boolean} upToDate 
+ */
+function eduMain(driver, upToDate)
+{
+  if (upToDate == false)
+  {
+    console.log("not uptodate")
+  }
+  else
+  {
+    console.log("up to date")
+  }
+}
 
 
 function main()
 {
-  driver = initDriver()
-  eduMobile(driver)
 
+
+  
 }
 
-function foo() {
-  document.getElementById("syncBtn").classList.add("rotation");
-} 
-
 main()
+
+// function foo() {
+//   document.getElementById("syncBtn").classList.add("rotation");
+// } 
