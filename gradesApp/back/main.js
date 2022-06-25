8767ce3c0a92a7a36f6d5ffcb92a567073c288ea
Puncher <65789180+Puncher1@users.noreply.github.com>
2022-06-25 15:13:14 +0200

    Line indentation to 4 spaces, scrapeData finished

--- gradesApp/back/main.js
+++ gradesApp/back/main.js
@@ -11,35 +11,33 @@ const prompt = require("prompt-sync")({ sigint: true })
  * @param {webdriver.WebElement} element 
  */
 async function getElementText(element) {
-  return new Promise(async resolve => {
-    await element.getText().then(function(elementText) {
-      resolve(elementText)
-    })
+    return new Promise(async resolve => {
+        await element.getText().then(function(elementText) {
+            resolve(elementText)
+        })
 
-  })
+    })
 
 }
 
 
 async function timeout(ms) {
-  return new Promise(res => setTimeout(res, ms))
+    return new Promise(res => setTimeout(res, ms))
 }
 
 
 async function initDriver() {
+    return new Promise(resolve => {
+        console.log("init")
+        let driver = new webdriver.Builder()
+            .forBrowser(Browser.CHROME)
+            .setChromeOptions(new chrome.Options()
+            .headless()
+            .windowSize({width: 1920, height: 1080}))
+            .build()
 
-  return new Promise(resolve => {
-    console.log("init")
-    let driver = new webdriver.Builder()
-    .forBrowser(Browser.CHROME)
-    .setChromeOptions(new chrome.Options()
-      .headless()
-      .windowSize({width: 1920, height: 1080}))
-    .build()
-
-    resolve(driver)
-  })
-
+        resolve(driver)
+    })
 }
 
 
@@ -49,34 +47,31 @@ async function initDriver() {
  * @param {webdriver.WebDriver} driver 
  */
 async function eduMobile(driver) {
+    return new Promise(async resolve => {
+        
+        // @ts-ignore
+        await driver.get(process.env.EDU_MOBILE_URL)
 
-  return new Promise(async resolve => {
-    
-    // @ts-ignore
-    await driver.get(process.env.EDU_MOBILE_URL)
+        const pinEntry = await driver.findElement(By.xpath('//*[@id="inputPin"]'))
+        // @ts-ignore
+        await pinEntry.sendKeys(process.env.EDU_PIN)
 
-    const pinEntry = await driver.findElement(By.xpath('//*[@id="inputPin"]'))
-    // @ts-ignore
-    await pinEntry.sendKeys(process.env.EDU_PIN)
-  
-    const pinOkBtn = await driver.findElement(By.xpath('/html/body/div/form/input[2]'))
-    await pinOkBtn.click()
+        const pinOkBtn = await driver.findElement(By.xpath('/html/body/div/form/input[2]'))
+        await pinOkBtn.click()
 
-    let mobileBody = await driver.findElement(By.xpath('/html/body'))
-    let upToDate = false
+        let mobileBody = await driver.findElement(By.xpath('/html/body'))
+        let upToDate = false
 
-    let mobiledBodyText = await getElementText(mobileBody)
-    console.log(mobiledBodyText)
-    if (mobiledBodyText.includes("Sie haben alle Noten bestätigt."))
-    {
-      console.log("if statement")
-      upToDate = true
-    }
-    
-    resolve(upToDate)
-  })
+        let mobiledBodyText = await getElementText(mobileBody)
+        console.log(mobiledBodyText)
+        if (mobiledBodyText.includes("Sie haben alle Noten bestätigt.")) {
+            console.log("if statement")
+            upToDate = true
+        }
+        
+        resolve(upToDate)
+    })
 }
-
 
 
 /**
@@ -85,102 +80,176 @@ async function eduMobile(driver) {
  */
 async function eduMain(driver) {
 
-  return new Promise(async resolve => {
+    return new Promise(async resolve => {
 
-    // @ts-ignore
-    await driver.get(process.env.EDU_URL)
+        // @ts-ignore
+        await driver.get(process.env.EDU_URL)
 
-    let otpNeeded = true
-  
-    let otpButton = await driver.findElement(By.xpath('//*[@id="form-container"]/div[5]/div/a/span'))
-      .then(null, function(err) {
-        if (err.name === "NoSuchElementError") {
-          otpNeeded = false
+        let otpNeeded = true
+
+        let otpButton = await driver.findElement(By.xpath('//*[@id="form-container"]/div[5]/div/a/span'))
+            .then(null, function(err) {
+                if (err.name === "NoSuchElementError") {
+                    otpNeeded = false
+
+                }
+            })
+        
+        if (otpNeeded) {
+            // @ts-ignore
+            await otpButton.click()
+        }
+
+        let usernameEntry = await driver.findElement(By.xpath('//*[@id="form-holder"]/div/form/div[1]/input'))
+        // @ts-ignore
+        await usernameEntry.sendKeys(process.env.EDU_USERNAME)
+
+        let passwordEntry = await driver.findElement(By.xpath('//*[@id="form-holder"]/div/form/div[2]/input'))
+        // @ts-ignore
+        await passwordEntry.sendKeys(process.env.EDU_PASSWORD)
+
+        let loginButton = await driver.findElement(By.xpath('//*[@id="form-actions"]/div/button'))
+        await loginButton.click()
+
+        if (otpNeeded) {
+            exec('"C:/Users/andri/AppData/Local/authy/Authy Desktop.exe"',
+                function (error, stdout, stderr) {
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
+                });
+
+            const otpInput = prompt("Please enter OTP: ")
+            console.log(otpInput)
+
+            let optEntry = await driver.findElement(By.xpath('//*[@id="form-holder"]/div/form/div[1]/input'))
+            await optEntry.sendKeys(otpInput)
+            
+            let optButton = await driver.findElement(By.xpath('//*[@id="form-actions"]/div/button'))
+            await optButton.click()
 
         }
-      })
-    
-    if (otpNeeded) {
-      // @ts-ignore
-      await otpButton.click()
-    }
-    let usernameEntry = await driver.findElement(By.xpath('//*[@id="form-holder"]/div/form/div[1]/input'))
-    // @ts-ignore
-    await usernameEntry.sendKeys(process.env.EDU_USERNAME)
+        
+        let gradesButton = await driver.findElement(By.xpath('//*[@id="menu21311"]'))
+        console.log("before grades button")
+        await gradesButton.click()
+        console.log("after grades button")
 
-    let passwordEntry = await driver.findElement(By.xpath('//*[@id="form-holder"]/div/form/div[2]/input'))
-    // @ts-ignore
-    await passwordEntry.sendKeys(process.env.EDU_PASSWORD)
-
-    let loginButton = await driver.findElement(By.xpath('//*[@id="form-actions"]/div/button'))
-    await loginButton.click()
-
-    if (otpNeeded) {
-      exec('"C:/Users/andri/AppData/Local/authy/Authy Desktop.exe"',
-        function (error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
-        });
-
-      const otpInput = prompt("Please enter OTP: ")
-      console.log(otpInput)
-
-      let optEntry = await driver.findElement(By.xpath('//*[@id="form-holder"]/div/form/div[1]/input'))
-      await optEntry.sendKeys(otpInput)
-      
-      let optButton = await driver.findElement(By.xpath('//*[@id="form-actions"]/div/button'))
-      await optButton.click()
-
-    }
-    
-    let gradesButton = await driver.findElement(By.xpath('//*[@id="menu21311"]'))
-    console.log("before grades button")
-    await gradesButton.click()
-    console.log("after grades button")
-
-    resolve(null)
-  })
+        resolve(null)
+    })
 
 }
 
 
 async function scrapeData(driver) {
-  return new Promise(async resolve => {
-    let gradesTable = await driver.findElement(By.xpath('//*[@id="uebersicht_bloecke"]/page/div/table'))
-    let subjectRows = await gradesTable.findElements(By.css('tr'))
-    
-    let subjectRowCount = 0
-    for (const subjectRow of subjectRows) {
-      let subjectRowText = await getElementText(subjectRow)
+    return new Promise(async resolve => {
+        let gradesTable = await driver.findElement(By.xpath('//*[@id="uebersicht_bloecke"]/page/div/table'))
+        let subjectRows = await gradesTable.findElements(By.css('tr'))
+        
+        let subjectRowCount = 0
+        for (const subjectRow of subjectRows) {
+            let subjectRowText = await getElementText(subjectRow)
+        
+            if (["Kurs Notendurchschnitt Bestätigt", ""].includes(subjectRowText)) {
+                continue
+            }
+            subjectRowCount++
+        }
 
-    }
+        let i = 0
+        while (i <= subjectRowCount) {
+            let notClicking = false
+            let buttonDetail = await driver.findElement(By.xpath(`//*[@id="einzelpr_btn_0_${i}"]`))
+                .then(null, function(err) {
+                if (err.name === "NoSuchElementError") {
+                    notClicking = true
+                }
+                })
+            
+            if (!notClicking) {
+                await buttonDetail.click()
+            }
 
-  })
-  
+            i++
+        }
+
+        subjectRows = []
+        let subjectRowCount_ = subjectRowCount
+        i = 0
+        while (i < subjectRowCount) {
+            let noContinue = false
+            let subjectElementTest = await driver.findElement(By.xpath('//*[@id="uebersicht_bloecke"]/page/div/table/tbody/tr[{i}]/td[4]'))
+                .then(null, function(err) {
+                    if (err.name === "NoSuchElementError") {
+                        subjectRowCount_++
+                        noContinue = true
+                    }
+                })
+            
+            if (!noContinue) {
+                noContinue = false
+                let subjectRow = await driver.findElement(By.xpath(`//*[@id="uebersicht_bloecke"]/page/div/table/tbody/tr[${i}]`))
+                    .then(null, function(err) {
+                        if (err.name === "NoSuchElementError") {
+                            subjectRows.push("")
+                            noContinue = true
+                        }
+                    })
+
+                if (!noContinue) {
+                    subjectRows.push(subjectRow)
+                }
+            }
+
+            i++
+        }
+
+        let detailRows = []
+        i = 3
+        while (i <= (subjectRowCount * 3)) {
+            let noContinue = false
+            try {
+                var detailTable = await driver.findElement(By.xpath(`//*[@id="uebersicht_bloecke"]/page/div/table/tbody/tr[${i}]/td/table`))
+                var detailTableRows = await detailTable.findElements(By.css("tr"))
+            }
+            catch(err) {
+                if (err.name === "NoSuchElementError") {
+                    detailRows.push("")
+                    noContinue = true
+                }
+            }
+            
+            if (!noContinue) {
+                detailRows.push(detailTableRows)
+            }
+
+            i += 3
+        }
+
+        console.log("subjectRows: " + subjectRows)
+        console.log("detailRows: " + detailRows)
+    })
 }
 
 
 async function main() {
-  
-  let driver = await initDriver()
-  console.log("after init")
-  let upToDate = await eduMobile(driver)
-  
-  console.log("after edu mobile: " + upToDate)
-  await eduMain(driver)
-  await scrapeData(driver)
 
-  // if (upToDate == false)
-  // {
-  //   console.log("not uptodate")
-  // }
-  // else
-  // {
-  //   console.log("up to date")
-  // }
+    let driver = await initDriver()
+    console.log("after init")
+    let upToDate = await eduMobile(driver)
+
+    console.log("after edu mobile: " + upToDate)
+    await eduMain(driver)
+    await scrapeData(driver)
+
+    // if (upToDate == false)
+    // {
+    //   console.log("not uptodate")
+    // }
+    // else
+    // {
+    //   console.log("up to date")
+    // }
 
 }
 
