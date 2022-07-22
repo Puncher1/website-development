a4f6f3a2d98425e873938c9d5b2e2e73631a732e
Puncher <65789180+Puncher1@users.noreply.github.com>
2022-07-22 16:03:54 +0200

    Login loading dots and empty fields handling

--- gradesApp/static/scripts/login.js
+++ gradesApp/static/scripts/login.js
@@ -2,26 +2,56 @@ let login = function () {
     let user = document.getElementById("user").value
     let pw = document.getElementById("pw").value
 
-    let data = {"request": {"type": "set", "data":  {"username": user, "password": pw}}}
-    fetch("http://localhost:5000/login", {             // origin: https://www.andrin-s.net/login
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data)
-    }).then(async res => {
-        let rawResponse = await res.json()
-        if (rawResponse["status"] == "invalid") {
-            document.getElementById("login-failed").innerHTML = "Invalid username or password."
-            document.getElementById("user").style.borderColor = "#9c0606"
-            document.getElementById("pw").style.borderColor = "#9c0606"
-        }
-        else if (rawResponse["status"] == "ok") {
-            document.getElementById("login-failed").innerHTML = ""
-            document.getElementById("user").style.borderColor = "#1c215e"
-            document.getElementById("pw").style.borderColor = "#1c215e"
-            window.location.replace("http://localhost:5000/grades")
-        }
+    let isUserEmpty = false
+    let isPwEmpty = false
+    if (user == "") {
+        document.getElementById("user").style.borderColor = "#9c0606"
+        isUserEmpty = true
+    }
+    else {
+        document.getElementById("user").style.borderColor = "#1c215e"
+    }
+    if (pw == "") {
+        document.getElementById("pw").style.borderColor = "#9c0606"
+        isPwEmpty = true
+    }
+    else {
+        document.getElementById("pw").style.borderColor = "#1c215e"
+    }
 
-    })
+    if (isUserEmpty || isPwEmpty) {
+        document.getElementById("login-failed").innerHTML = "This field is required."
+    }
+    else {
+        document.getElementById("submitBtn").disabled = true
+        document.getElementById("loading-dots").style.display = "block"
+        document.getElementById("login-failed").innerHTML = ""
+        document.getElementById("user").style.borderColor = "#1c215e"
+        document.getElementById("pw").style.borderColor = "#1c215e"
+    
+        let data = {"request": {"type": "set", "data":  {"username": user, "password": pw}}}
+        fetch("http://localhost:5000/login", {             // origin: https://www.andrin-s.net/login
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        }).then(async res => {
+            let rawResponse = await res.json()
+            document.getElementById("submitBtn").disabled = false
+            document.getElementById("loading-dots").style.display = "none"
+            if (rawResponse["status"] == "invalid") {
+                document.getElementById("login-failed").innerHTML = "Invalid username or password."
+                document.getElementById("user").style.borderColor = "#9c0606"
+                document.getElementById("pw").style.borderColor = "#9c0606"
+            }
+            else if (rawResponse["status"] == "ok") {
+                document.getElementById("login-failed").innerHTML = ""
+                document.getElementById("user").style.borderColor = "#1c215e"
+                document.getElementById("pw").style.borderColor = "#1c215e"
+                window.location.replace("http://localhost:5000/grades")
+            }
+    
+        })
+    }
 }
 
 
